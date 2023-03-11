18f43118819d9026df7bee9bc16c3ff2bf5aaa42
Egor <egortokarev59@gmail.com>
2023-03-11 17:32:19 +0300

    ClientApp

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,13 @@
+const { test, expect } = require('@playwright/test');
+
+test('Browser Context Playright test', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/client');
+    await page.locator('#userEmail').fill("anshika@gmail.com");
+    await page.locator('#userPassword').type("Iamking@000", { delay: 100 });  //delay - time to print
+    await page.locator("[value='Login']").click();
+    await page.waitForLoadState('domcontentloaded');
+  //  await page.waitForTimeout(3000);
+    const titles = await page.locator(".card-body b").allTextContents();
+    console.log(titles); 
+    console.log('waitForTimeout'); 
+});
