9fc1d5d2c057da9328d67e3048dd80887942dd74
Egor <egortokarev59@gmail.com>
2023-03-11 17:33:58 +0300

    ClientApp2

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -3,10 +3,10 @@ const { test, expect } = require('@playwright/test');
 test('Browser Context Playright test', async ({ page }) => {
     await page.goto('https://rahulshettyacademy.com/client');
     await page.locator('#userEmail').fill("anshika@gmail.com");
-    await page.locator('#userPassword').type("Iamking@000", { delay: 100 });  //delay - time to print
+    await page.locator('#userPassword').type("Iamking@000", { delay: 400 });  //delay - time to print
     await page.locator("[value='Login']").click();
-    await page.waitForLoadState('domcontentloaded');
-  //  await page.waitForTimeout(3000);
+ // await page.waitForLoadState('domcontentloaded'); not work 
+    await page.waitForTimeout(3000); 
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles); 
     console.log('waitForTimeout'); 
